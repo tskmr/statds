6955f855d31bc42a2a155d3d6273bcbf334c4bce
Takenori Sakumura <sakumura.takenori@gmail.com>
2023-02-15 10:40:00 +0900

    230215

--- R/setup.R
+++ R/setup.R
@@ -1,20 +1,17 @@
 setup = function(){
   setwd(dirname(rstudioapi::getActiveDocumentContext()$path)); rm(list = ls())
   if(!require(tidyverse)) install.packages("tidyverse"); library(tidyverse)
-  if(!require(scales)) install.packages("scales"); library(scales);
-  if(!require(rstan)) install.packages("rstan"); library(rstan)
-  rstan_options(auto_write = TRUE)
-  options(mc.cores = parallel::detectCores())
-  rstan_options(javascript = FALSE)
-  if(!require(lubridate)) install.packages("lubridate"); library(lubridate)
+  if(!require(magrittr)) library(magrittr)
   if(!require(ggridges)) install.packages("ggridges"); library(ggridges)
   if(!require(directlabels)) install.packages("directlabels"); library(directlabels)
+  #
+  if(!require(scales)) install.packages("scales"); library(scales);
+  if(!require(lubridate)) install.packages("lubridate"); library(lubridate)
   # install.packages("systemfonts", "miniUI", "DT")
   if(!require(systemfonts)) install.packages(systemfonts); library(systemfonts)
   if(!require(miniUI)) install.packages("miniUI"); library(miniUI)
   if(!require(DT)) install.packages("DT"); library(DT)
   capabilities()[c("cairo", "X11")]
-  # remotes::install_github("Gedevan-Aleksizde/fontregisterer", repos = NULL, type = "source")
   if(!require(fontregisterer)) remotes::install_github("Gedevan-Aleksizde/fontregisterer", repos = NULL, type = "source"); library(fontregisterer)
   sans = fontregisterer::get_standard_ja_fonts()[1]
   theme_set(theme(text = element_text(family = sans),
@@ -25,4 +22,8 @@ setup = function(){
                   axis.text = element_text(family = sans)))
   update_geom_defaults("text", list(family = sans))
   # theme_set(mytheme_bw(36)+theme(axis.title.x = element_text(family = "serif"),axis.title.y = element_text(family = "serif")))
+  if(!require(rstan)) install.packages("rstan"); library(rstan)
+  rstan_options(auto_write = TRUE)
+  options(mc.cores = parallel::detectCores())
+  rstan_options(javascript = FALSE)
 }
